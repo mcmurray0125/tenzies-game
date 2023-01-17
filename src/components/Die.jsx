75cb0f875bdc2664d698127735eeeadff3edf2cd
Michael Murray <mcmurray0125@gmail.com>
2023-01-16 22:46:42 -0600

    added dice SVGs instead of numbers for dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,10 +1,10 @@
-
+import React from "react"
 
 export default function Die(props) {
 
     return (
-        <div className="die-face" style={{backgroundColor: props.isHeld ? '#59E391' : 'white' }} onClick={props.holdDice}>
-            <h2 className="die-num">{props.value}</h2>
-        </div>
+        <img className={`${props.isHeld ? 'die-face-green' : 'die-face'}`}
+        src={`/src/images/${props.value}.svg`} onClick={props.holdDice}>
+        </img>
     )
 }
